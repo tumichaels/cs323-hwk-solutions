366b07d2262953d34f8bd73d9512ae76028a9f68
Tu Michael <mst45@zoo.cs.yale.edu>
2022-09-22 16:27:08 -0400

    pass first 15 tests

--- proj2/dmalloc/dmalloc.cc
+++ proj2/dmalloc/dmalloc.cc
@@ -8,14 +8,19 @@
 
 // You may write code here.
 // (Helper functions, types, structs, macros, globals, etc.)
-unsigned long long nactive = 0;		// number of active allocations [#malloc - #free]
-unsigned long long active_size = 0; 	// number of bytes in active allocations
-unsigned long long ntotal = 0;      	// number of allocations, total
-unsigned long long total_size = 0;  	// number of bytes in allocations, total
-unsigned long long nfail = 0;       	// number of failed allocation attempts
-unsigned long long fail_size = 0;   	// number of bytes in failed allocation attempts
-uintptr_t heap_min = 0;             	// smallest address in any region ever allocated
-uintptr_t heap_max = 0;             	// largest address in any region ever allocated
+
+typedef unsigned long long Header;
+
+unsigned long long nactive = 0;				// number of active allocations [#malloc - #free]
+unsigned long long active_size = 0;			// number of bytes in active allocations
+unsigned long long ntotal = 0;				// number of allocations, total
+unsigned long long total_size = 0;			// number of bytes in allocations, total
+unsigned long long nfail = 0;				// number of failed allocation attempts
+unsigned long long fail_size = 0;			// number of bytes in failed allocation attempts
+										
+// TODO: there's a better way to deal with these
+uintptr_t heap_min = 0;						// smallest address in any region ever allocated
+uintptr_t heap_max = 0;						// largest address in any region ever allocated
 
 /// dmalloc_malloc(sz, file, line)
 ///    Return a pointer to `sz` bytes of newly-allocated dynamic memory.
@@ -24,29 +29,45 @@ uintptr_t heap_max = 0;             	// largest address in any region ever alloc
 ///    request was at location `file`:`line`.
 
 void* dmalloc_malloc(size_t sz, const char* file, long line) {
-	(void) file, (void) line;   // avoid uninitialized variable warnings
-				    // Your code here.
-	
-	// i'm not doing anything smart with this
-	void *out = base_malloc(sz);
+    (void) file, (void) line;   // avoid uninitialized variable warnings
+    // Your code here.
+
+	void *out;
+
+	// src stack overflow: https://tinyurl.com/43kkycew
+	size_t malloc_size;
+	if (__builtin_add_overflow(sz, sizeof(Header), &malloc_size)){
+		out = NULL;
+	} else {
+		out = base_malloc(malloc_size);
+	}
+
+	if (out == NULL) {
+		nfail += 1;
+		fail_size += sz;
+		return out;
+	}
+
+	// set the header to size
+	((Header *) out)[0] = sz;
 
 	nactive += 1;
 	active_size += sz;
 	ntotal += 1;
 	total_size += sz;
-	
-	// I'm assuming we don't fail
 
-	if (ntotal == 0) {
+	out = (Header *)out + 1; // actual address given to user 
+
+	if (ntotal == 1) {
 		heap_min = (uintptr_t) out;
-		heap_max = (uintptr_t) out;
+		heap_max = (uintptr_t) out + sz;
 	} else if ((uintptr_t) out < heap_min) {
 		heap_min = (uintptr_t) out;
-	} else if ((uintptr_t) out > heap_max) {
-		heap_max = (uintptr_t) out;
+	} else if ((uintptr_t) out + sz > heap_max) {
+		heap_max = (uintptr_t) out + sz;
 	}
 
-	return out;
+    return out; 
 }
 
 
@@ -56,19 +77,16 @@ void* dmalloc_malloc(size_t sz, const char* file, long line) {
 ///    does nothing. The free was called at location `file`:`line`.
 
 void dmalloc_free(void* ptr, const char* file, long line) {
-	(void) file, (void) line;   // avoid uninitialized variable warnings
-				    // Your code here.
+    (void) file, (void) line;   // avoid uninitialized variable warnings
+    // Your code here.
 	
-	// I'm not doing any error checking for a bad pointer
-	base_free(ptr);
+	if (ptr == NULL)
+		return;
+
+	Header sz = ((Header *)ptr - 1)[0];
 	
 	nactive -= 1;
-	// can't do active size yet
-	// eerything else isn't updated
-
-	// also not worrying about updating these
-	if ((uintptr_t) ptr < heap_min){;}
-	else if ((uintptr_t) ptr > heap_max){;}
+	active_size -= sz;
 }
 
 
@@ -80,12 +98,18 @@ void dmalloc_free(void* ptr, const char* file, long line) {
 ///    location `file`:`line`.
 
 void* dmalloc_calloc(size_t nmemb, size_t sz, const char* file, long line) {
-	// Your code here (to fix test014).
-	void* ptr = dmalloc_malloc(nmemb * sz, file, line);
-	if (ptr) {
-		memset(ptr, 0, nmemb * sz);
+	size_t malloc_size;
+	if (__builtin_mul_overflow(nmemb, sz, &malloc_size)) {
+		nfail += 1;
+		fail_size += malloc_size;
+		return NULL;
 	}
-	return ptr;
+
+	void* ptr = dmalloc_malloc(nmemb * sz, file, line);
+    if (ptr) {
+        memset(ptr, 0, nmemb * sz);
+    }
+    return ptr;
 }
 
 
@@ -93,32 +117,30 @@ void* dmalloc_calloc(size_t nmemb, size_t sz, const char* file, long line) {
 ///    Store the current memory statistics in `*stats`.
 
 void dmalloc_get_statistics(dmalloc_statistics* stats) {
-	// Stub: set all statistics to enormous numbers
-	memset(stats, 255, sizeof(dmalloc_statistics));
-	// Your code here.
+    // Stub: set all statistics to enormous numbers
+    memset(stats, 255, sizeof(dmalloc_statistics));
 	stats->nactive = nactive;
 	stats->active_size = active_size;
 	stats->ntotal = ntotal;
 	stats->total_size = total_size;
 	stats->nfail = nfail;
-	stats->fail_size = fail_size; 
+	stats->fail_size = fail_size;
 	stats->heap_min = heap_min;
 	stats->heap_max = heap_max;
 }
-
 
 
 /// dmalloc_print_statistics()
 ///    Print the current memory statistics.
 
 void dmalloc_print_statistics() {
-	dmalloc_statistics stats;
-	dmalloc_get_statistics(&stats);
+    dmalloc_statistics stats;
+    dmalloc_get_statistics(&stats);
 
-	printf("alloc count: active %10llu   total %10llu   fail %10llu\n",
-			stats.nactive, stats.ntotal, stats.nfail);
-	printf("alloc size:  active %10llu   total %10llu   fail %10llu\n",
-			stats.active_size, stats.total_size, stats.fail_size);
+    printf("alloc count: active %10llu   total %10llu   fail %10llu\n",
+           stats.nactive, stats.ntotal, stats.nfail);
+    printf("alloc size:  active %10llu   total %10llu   fail %10llu\n",
+           stats.active_size, stats.total_size, stats.fail_size);
 }
 
 
@@ -127,7 +149,7 @@ void dmalloc_print_statistics() {
 ///    memory.
 
 void dmalloc_print_leak_report() {
-	// Your code here.
+    // Your code here.
 }
 
 
@@ -135,5 +157,5 @@ void dmalloc_print_leak_report() {
 ///    Print a report of heavily-used allocation locations.
 
 void dmalloc_print_heavy_hitter_report() {
-	// Your heavy-hitters code here
+    // Your heavy-hitters code here
 }

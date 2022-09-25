456be878a9725ddef9c74b40b41a7cb149f1630c
Tu Michael <mst45@zoo.cs.yale.edu>
2022-09-25 08:05:14 -0400

    passed all test cases

--- proj2/dmalloc/dmalloc.cc
+++ proj2/dmalloc/dmalloc.cc
@@ -9,20 +9,16 @@
 // You may write code here.
 // (Helper functions, types, structs, macros, globals, etc.)
 
-// these macro funcs act on the ptr provided to the user
+// these macro funcs act on the ptr provided to the user 
 #define OFFSET 256
 #define UCANARY(ptr) (*((size_t *)ptr - 1))
-#define META(ptr) (*((Meta *)((uintptr_t)ptr - OFFSET - sizeof(size_t)) - 1))
-#define SENTINEL(ptr) (*((size_t *)((uintptr_t)ptr - OFFSET - sizeof(size_t))))
-#define OCANARY(ptr) (*((size_t *)((uintptr_t)ptr + META(ptr)->sz)))
+#define HEAD(ptr) (*((Header *)((uintptr_t)&UCANARY(ptr) - OFFSET) - 1))
+#define OCANARY(ptr) (*((size_t *)((uintptr_t)ptr + HEAD(ptr).sz)))
 
-#define CANARY_SIZE sizeof(unsigned long long)
+#define CANARY_SIZE sizeof(size_t)
 #define CANARY_VAL 0xdeadbeefdeadbeef // on 32 bit, it's trimmed to deadbeef
 
-#define UNFREED_VAL 0xbeadbabebeadbabe
-#define SENTINEL_VAL 0xfadedeaffadedeaf
-
-struct meta {
+typedef struct header {
 	// basic stats
 	size_t sz;
 	const char *file;
@@ -34,9 +30,9 @@ struct meta {
 	void *next;
 
 	// free protections
-	size_t freed;
-};
-typedef struct meta *Meta;
+	bool freed;
+	unsigned int present;
+} Header;
 
 void *last_active = 0x0;
 
@@ -56,52 +52,48 @@ uintptr_t heap_max = 0;						// largest address in any region ever allocated
 ///    request was at location `file`:`line`.
 
 void* dmalloc_malloc(size_t sz, const char* file, long line) {
-	(void) file, (void) line;   // avoid uninitialized variable warnings
-								// Your code here.
+    (void) file, (void) line;   // avoid uninitialized variable warnings
+    // Your code here.
+
 	void *out;
-	Meta m;
 
 	// src stack overflow: https://tinyurl.com/43kkycew
 	size_t malloc_size;
-	if (__builtin_add_overflow(sz, sizeof(Meta) + OFFSET + 2*CANARY_SIZE, &malloc_size)){
+	if (__builtin_add_overflow(sz, sizeof(Header)+OFFSET + 2*CANARY_SIZE, &malloc_size)){
 		out = NULL;
-		m = NULL;
 	} else {
 		out = base_malloc(malloc_size);
-		m = (Meta)base_malloc(sizeof(struct meta));
 	}
 
-	if (out == NULL || m == NULL) {
+	if (out == NULL) {
 		nfail += 1;
 		fail_size += sz;
 		return out;
 	}
 
-	// fill out the metadata 
-	m->sz = sz;
-	m->file = file;
-	m->line = line;
-	m->prev = last_active;
-	m->self = NULL;
-	m->next = NULL;
-	m->freed = UNFREED_VAL;
-
-	// load metadata address into leading portion of out
-	*(Meta *)out = m;
+	// fill out the header 
+	Header* h = (Header *)out;
+	h->sz = sz;
+	h->file = file;
+	h->line = line;
+	h->prev = last_active;
+	h->self = NULL;
+	h->next = NULL;
+	h->freed = false;
+	h->present = 0xdeadbeef;
 
 	// start of returned block
-	out = (void *)((uintptr_t)out + sizeof(Meta) + OFFSET + CANARY_SIZE);
-
-	// set canaries and sentinel
-	SENTINEL(out) = SENTINEL_VAL;
-	UCANARY(out) = CANARY_VAL;
-	OCANARY(out) = CANARY_VAL;
+	out = (void *)((uintptr_t)out + sizeof(Header) + OFFSET + CANARY_SIZE);
 
 	// update ll and last_active
-	if (m->prev)
-		META(m->prev)->next = out;
-	m->self = out;
+	if (h->prev)
+		HEAD(h->prev).next = out;
+	h->self = out;
 	last_active = out;
+
+	// set canaries
+	UCANARY(out) = CANARY_VAL;
+	OCANARY(out) = CANARY_VAL;
 
 	// update stats
 	nactive += 1;
@@ -118,7 +110,7 @@ void* dmalloc_malloc(size_t sz, const char* file, long line) {
 		heap_max = (uintptr_t) out + sz;
 	}
 
-	return out; 
+    return out; 
 }
 
 
@@ -128,9 +120,10 @@ void* dmalloc_malloc(size_t sz, const char* file, long line) {
 ///    does nothing. The free was called at location `file`:`line`.
 
 void dmalloc_free(void* ptr, const char* file, long line) {
-	(void) file, (void) line;   // avoid uninitialized variable warnings
-								// Your code here.
-
+    (void) file, (void) line;   // avoid uninitialized variable warnings
+    // Your code here.
+	
+	
 	if (ptr == NULL)
 		return;
 
@@ -141,32 +134,27 @@ void dmalloc_free(void* ptr, const char* file, long line) {
 		// additional error message about outside of heap free 
 		exit(1);
 
-	} else if (SENTINEL(ptr) != SENTINEL_VAL) {
-		void *temp = last_active;
-		assert(temp);
-
-		while (temp){
-			if (ptr == temp) {
-				break;
-			} else if ((uintptr_t)temp < (uintptr_t)ptr && (uintptr_t)ptr < (uintptr_t)temp + META(temp)->sz) {
-				fprintf(stdout, "MEMORY BUG: %s:%ld: invalid free of pointer %p, not allocated\n", 
-						file, line, ptr);
-				fprintf(stdout, "  %s:%ld: %p is %zu bytes inside a %zu byte region allocated here\n", 
-						META(temp)->file, META(temp)->line, ptr, (uintptr_t)ptr - (uintptr_t)temp, META(temp)->sz);
-				exit(1);
-			}
-			temp = META(temp)->prev;
-		}
-
-		if (temp == NULL) {
-			fprintf(stdout, "%s:%ld: %p is outside of any allocated block\n", 
-					file, line, ptr); 
-			exit(1);
-		}
-
-	} else if (META(ptr)->freed != UNFREED_VAL) {
+	} else if (HEAD(ptr).freed) {
 		fprintf(stdout, "MEMORY BUG: %s:%ld: invalid free of pointer %p, double free\n", 
 				file, line, ptr);
+		exit(1);
+	
+	} else if (HEAD(ptr).present != 0xdeadbeef || HEAD(ptr).self != ptr) {
+		fprintf(stdout, "MEMORY BUG: %s:%ld: invalid free of pointer %p, not allocated\n", 
+				file, line, ptr);
+
+		void *temp = last_active;
+		assert(temp);
+		while (temp){
+			if ((uintptr_t)temp < (uintptr_t)ptr && (uintptr_t)ptr < (uintptr_t)temp + HEAD(temp).sz) {
+				fprintf(stdout, "  %s:%ld: %p is %zu bytes inside a %zu byte region allocated here", 
+						HEAD(temp).file, HEAD(temp).line, ptr, (uintptr_t)ptr - (uintptr_t)temp, HEAD(temp).sz);
+				exit(1);
+			}
+			temp = HEAD(temp).prev;
+		}
+		fprintf(stdout, "%s:%ld: %p is outside of any allocated block\n", 
+				file, line, ptr); 
 		exit(1);
 	}
 
@@ -177,25 +165,21 @@ void dmalloc_free(void* ptr, const char* file, long line) {
 	}
 
 	// update ll and last active
-	if (META(ptr)->prev) 
-		META(META(ptr)->prev)->next = META(ptr)->next;
-	if (META(ptr)->next) 
-		META(META(ptr)->next)->prev = META(ptr)->prev;
+	if (HEAD(ptr).prev) 
+		HEAD(HEAD(ptr).prev).next = HEAD(ptr).next;
+	if (HEAD(ptr).next) 
+		HEAD(HEAD(ptr).next).prev = HEAD(ptr).prev;
 	if (ptr == last_active) {
-		assert(META(ptr)->next == NULL);
-		last_active = META(ptr)->prev;
+		assert(HEAD(ptr).next == NULL);
+		last_active = HEAD(ptr).prev;
 	}
 
-	// modify the metadata
-	META(ptr)->freed = 0x0;
+	// modify the header
+	HEAD(ptr).freed = true;
 
 	// update overall data 
 	nactive -= 1;
-	active_size -= META(ptr)->sz;
-
-	// free the pointer and its metadata
-	base_free(ptr);
-	base_free(META(ptr));
+	active_size -= HEAD(ptr).sz;
 }
 
 
@@ -215,10 +199,10 @@ void* dmalloc_calloc(size_t nmemb, size_t sz, const char* file, long line) {
 	}
 
 	void* ptr = dmalloc_malloc(nmemb * sz, file, line);
-	if (ptr) {
-		memset(ptr, 0, nmemb * sz);
-	}
-	return ptr;
+    if (ptr) {
+        memset(ptr, 0, nmemb * sz);
+    }
+    return ptr;
 }
 
 
@@ -226,8 +210,8 @@ void* dmalloc_calloc(size_t nmemb, size_t sz, const char* file, long line) {
 ///    Store the current memory statistics in `*stats`.
 
 void dmalloc_get_statistics(dmalloc_statistics* stats) {
-	// Stub: set all statistics to enormous numbers
-	memset(stats, 255, sizeof(dmalloc_statistics));
+    // Stub: set all statistics to enormous numbers
+    memset(stats, 255, sizeof(dmalloc_statistics));
 	stats->nactive = nactive;
 	stats->active_size = active_size;
 	stats->ntotal = ntotal;
@@ -243,13 +227,13 @@ void dmalloc_get_statistics(dmalloc_statistics* stats) {
 ///    Print the current memory statistics.
 
 void dmalloc_print_statistics() {
-	dmalloc_statistics stats;
-	dmalloc_get_statistics(&stats);
+    dmalloc_statistics stats;
+    dmalloc_get_statistics(&stats);
 
-	printf("alloc count: active %10llu   total %10llu   fail %10llu\n",
-			stats.nactive, stats.ntotal, stats.nfail);
-	printf("alloc size:  active %10llu   total %10llu   fail %10llu\n",
-			stats.active_size, stats.total_size, stats.fail_size);
+    printf("alloc count: active %10llu   total %10llu   fail %10llu\n",
+           stats.nactive, stats.ntotal, stats.nfail);
+    printf("alloc size:  active %10llu   total %10llu   fail %10llu\n",
+           stats.active_size, stats.total_size, stats.fail_size);
 }
 
 
@@ -258,12 +242,12 @@ void dmalloc_print_statistics() {
 ///    memory.
 
 void dmalloc_print_leak_report() {
-	// Your code here.
+    // Your code here.
 	void *temp = last_active;
 	while(temp) {
 		fprintf(stdout, "LEAK CHECK: %s:%ld: allocated object %p with size %zu\n", 
-				META(temp)->file, META(temp)->line, temp, META(temp)->sz);
-		temp = META(temp)->prev;
+				HEAD(temp).file, HEAD(temp).line, temp, HEAD(temp).sz);
+		temp = HEAD(temp).prev;
 	}
 }
 
@@ -272,5 +256,5 @@ void dmalloc_print_leak_report() {
 ///    Print a report of heavily-used allocation locations.
 
 void dmalloc_print_heavy_hitter_report() {
-	// Your heavy-hitters code here
+    // Your heavy-hitters code here
 }
